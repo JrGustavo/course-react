e991cd5ad013565588451e934378081dce0b29c9
JrGustavo <gus@soypetal.com>
2023-07-10 19:20:41 -0500

    probando vite

---
+++ src/Main.jsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import ReactDOM from 'react-dom/client'
+
+function App(){
+	return <h1>tengo popo </h1>
+}
+
+ReactDOM.createRoot( document.getElementById('root')).render(
+	<React.StrictMode>
+		<App/>
+	</React.StrictMode>
+);
+
